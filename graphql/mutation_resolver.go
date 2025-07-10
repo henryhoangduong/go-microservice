7a1a86baffb8d53c0d7a74c86ecaba0885572893
henryhoangduong <henryhoangduong@gmail.com>
2025-07-11 06:30:44 +0700

    NewGraphqlServer

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,1 +1,16 @@
 package main
+
+type mutationResolver struct {
+	server *Server
+}
+
+// func (r *mutationResolver) CreateAccount(ctx context.Context, in AccountInput) (*Account, error) {
+
+// }
+
+// func (r *mutationResolver) CreateProduct(ctx context.Context, in ProductInput) (*Product, error) {
+
+// }
+// func (r *mutationResolver) CreateOrder(ctx context.Context, in OrderInput) (*Order, error) {
+
+// }
--- graphql/query_resolver.go
+++ graphql/query_resolver.go
@@ -1,1 +1,12 @@
 package main
+
+type queryResolver struct {
+	server *Server
+}
+
+// func (r *queryResolver) Accounts(ctx context.Context, paginaiton *PaginationInput, id *string) ([]*Account, error) {
+
+// }
+// func (r *queryResolver) Products(ctx context.Context, paginaiton *PaginationInput, query *string, id *string) ([]*Account, error) {
+
+// }
