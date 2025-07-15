d899bdc182e973a2a159e10bab9a4e51f54039ae
henryhoangduong <henryhoangduong@gmail.com>
2025-07-15 19:15:06 +0700

    catalog

--- catalog/service.go
+++ catalog/service.go
@@ -13,17 +13,20 @@ type Product struct {
 	Price       float64 `json:"price"`
 }
 type Service interface {
-	PostProduct(ctx *context.Context, name, desctiption string, price float64) (*Product, error)
-	GetProduct(ctx *context.Context, id string) (*Product, error)
-	GetProducts(ctx *context.Context, skip uint64, take uint) ([]Product, error)
-	GetProductsByIDs(ctx *context.Context, ids []string) ([]Product, error)
-	SearchProducts(ctx *context.Context, query string, skip uint64, take uint64) ([]Product, error)
+	PostProduct(ctx context.Context, name, description string, price float64) (*Product, error)
+	GetProduct(ctx context.Context, id string) (*Product, error)
+	GetProducts(ctx context.Context, skip uint64, take uint64) ([]Product, error)
+	GetProductsByIDs(ctx context.Context, ids []string) ([]Product, error)
+	SearchProducts(ctx context.Context, query string, skip uint64, take uint64) ([]Product, error)
 }
 
 type catalogService struct {
 	repository Repository
 }
 
+func NewService(r Repository) Service {
+	return &catalogService{r}
+}
 func (s *catalogService) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error) {
 	p := &Product{
 		Name:        name,
